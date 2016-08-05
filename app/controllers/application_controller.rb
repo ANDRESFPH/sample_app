aea23bb6ed7f202f029966cf3001afb4f40fa10e
Andres <andresfph1989@gmail.com>
2016-08-05 16:12:01 -0500

    fix error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    root render :html "hello welcome to the sample app"	
+    render :html "hello welcome to the sample app"	
   end
 end
